34b4ba12d69f309732f4d4917f151f4820665501
Dmitriy Kirov <kirovdmitriy@gmail.com>
2024-01-20 17:19:50 +0200

    update

--- Week6/Day3/Exercise 6/script.js
+++
@@ -1,9 +0,0 @@
-let jobTitle = prompt("What is your job name?");
-let geoLocation = prompt("Where do you live(country)?");
-let partnerName = prompt("Enter yours wife/husband name");
-let amountOfChildren = prompt("how many children do you have?");
-(function (jobTitle, geoLocation, partnerName, amountOfChildren) {
-  const p = document.createElement("p");
-  p.textContent = `You will be a ${jobTitle} in ${geoLocation}, and married to ${partnerName} with ${amountOfChildren} kids.`;
-  document.body.append(p);
-})(jobTitle, geoLocation, partnerName, amountOfChildren);
